863d084bf5c302dac69ecbb1cb849fbb260618ee
Kyle Arch <kymagic@gmail.com>
2016-12-23 16:59:23 +0000

    Remove testing code

--- ebooks/__init__.py
+++ ebooks/__init__.py
@@ -3,8 +3,6 @@ from twitterapi import TwitterApi
 
 def main():
     twapi = TwitterApi()
-    twapi.tweet("Tweep Tweep", twapi.BOT)
-    twapi.tweet("Twoop Twoop", twapi.SRC)
 
 if __name__ == '__main__':
     main()
