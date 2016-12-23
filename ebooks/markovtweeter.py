d44800e32230dc685f1d617827669cdfb3702a1c
Kyle Arch <kymagic@gmail.com>
2016-12-23 17:44:02 +0000

    Add empty class and simple doc for markov class

---
+++ ebooks/markovtweeter.py
@@ -0,0 +1,9 @@
+
+"""
+Markov-like (probably) tweet generator.
+
+Source file is expected at ./tweets.csv , which any twitter user can download
+as their Twitter Archive.
+"""
+class MarkovTweeter:
+    pass
