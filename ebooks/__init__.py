0ee84eba306b34747cc881ebc9000a08db03d038
Kyle Arch <kymagic@gmail.com>
2016-12-27 17:48:15 +0000

    Include more sensible init file

--- ebooks/__init__.py
+++ ebooks/__init__.py
@@ -6,6 +6,7 @@ from tweetloader import TweetLoader
 def main():
     loader = TweetLoader()
     generator = MarkovTweetGenerator(loader.get_tweets())
+    print(generator.get_formatted_tweet())
 
 if __name__ == '__main__':
     main()
