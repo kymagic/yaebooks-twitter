e684d9d764456559df8b9716193302e3529f69e5
Kyle Arch <kymagic@gmail.com>
2016-12-29 16:42:58 +0000

    Add command line arguments

--- ebooks/__init__.py
+++ ebooks/__init__.py
@@ -1,12 +1,39 @@
 from markovtweetgenerator import MarkovTweetGenerator
 from twitterapi import TwitterApi
 from tweetloader import TweetLoader
+from argparse import ArgumentParser
 
 
 def main():
+
+    args = parse_arguments()
+
     loader = TweetLoader()
     generator = MarkovTweetGenerator(loader.get_tweets())
-    print(generator.get_formatted_tweet())
+    tweet = generator.get_formatted_tweet()
+
+    if args.print:
+        print(tweet)
+
+    if args.tweet:
+        twitter_api = TwitterApi()
+        twitter_api.tweet(tweet)
+
+def parse_arguments():
+
+    parser = ArgumentParser()
+
+    output_opts = parser.add_argument_group('output options')
+
+    output_opts.add_argument('-t', '--tweet', action='store_true', help='Generated tweet is tweeted via the configured API')
+    output_opts.add_argument('-p', '--print', action='store_true', help='Generated tweet is printed to stdout')
+
+    args = parser.parse_args()
+
+    if not (args.tweet or args.print):
+        ArgumentParser.exit(1, 'At least one of --tweet or --print must be specified')
+
+    return args
 
 if __name__ == '__main__':
     main()
